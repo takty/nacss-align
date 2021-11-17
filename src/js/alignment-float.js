39ac7fbb91d848db148e144d9b315e6b8a24f89d
Takuto Yanagida <yanagida@stxst.com>
2021-11-17 14:39:37 +0900

    Refactoring

---
+++ src/js/alignment-float.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Alignment - Float
+ *
+ * @author Takuto Yanagida
+ * @version 2021-11-17
+ *
+ */
+
+
+window['NACSS'] = window['NACSS'] || {};
+
+
+(function (NS) {
+
+	(function () {
+		// @include _float.js
+		NS.alignmentFloat = initialize;
+	})();
+
+	// @include _style-class.js
+	// @include _utilities.js
+
+})(window['NACSS']);
---
+++ src/js/alignment-sel-attr.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Alignment - Selector Attribute
+ *
+ * @author Takuto Yanagida
+ * @version 2021-11-17
+ *
+ */
+
+
+window['NACSS'] = window['NACSS'] || {};
+
+
+(function (NS) {
+
+	(function () {
+		// @include _sel-attr.js
+		NS.alignmentSelectorAttribute = initialize;
+	})();
+
+	// @include _style-class.js
+	// @include _utilities.js
+
+})(window['NACSS']);
